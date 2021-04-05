a183e8ce5fdc364588c1ea1c6ab375e89798ccc7
Suejin Kim <kim.suejin.20@gmail.com>
2021-04-05 12:27:40 -0400

    changed success message

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {    
     fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log('Success!')
+        err ? console.error(err) : console.log('Success! Your ReadMe has been generated!')
     );
 }
 
